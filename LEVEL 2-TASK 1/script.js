c6c204ab0831901c2288d00ec9eb6696784a3731
SOURAV <102719151+sourav1008@users.noreply.github.com>
2023-06-15 10:33:33 +0530

    Add files via upload

---
+++ LEVEL 2-TASK 1/script.js
@@ -0,0 +1,14 @@
+function appendToResult(value) {
+    document.getElementById('result').value += value;
+  }
+  
+  function clearResult() {
+    document.getElementById('result').value = '';
+  }
+  
+  function calculateResult() {
+    var result = document.getElementById('result').value;
+    var calculatedResult = eval(result);
+    document.getElementById('result').value = calculatedResult;
+  }
+  
